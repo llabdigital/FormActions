a6927f32dfaec4aba864fdd36069e3d39ea7214a
your_username <your_email@example.com>
2026-04-03 09:59:09 -0700

    change the text to be conditional based on pending

--- src/components/Submit.jsx
+++ src/components/Submit.jsx
@@ -1,10 +1,12 @@
 import { useFormStatus } from 'react-dom';
 export default function Submit() {
-    const {pending } = useFormStatus();
+  const { pending } = useFormStatus();
 
-    return (
-        <p className="actions">
-          <button type="submit" disabled={pending}>Submit</button>
-        </p>
-    )
+  return (
+    <p className="actions">
+      <button type="submit" disabled={pending}>
+        {pending ? 'Submitting...' : 'Submit'}
+      </button>
+    </p>
+  );
 }
